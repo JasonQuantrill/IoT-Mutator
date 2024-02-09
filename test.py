c71819aafe5f6d162efcfe561df9d5adec9aedc8
JasonQuantrill <91751222+JasonQuantrill@users.noreply.github.com>
2024-02-09 12:34:25 -0500

    main update test script

--- test.py
+++ test.py
@@ -2,28 +2,52 @@ import os
 import unittest
 import subprocess
 
-class TestStringMethods(unittest.TestCase):
-    folder = 'C:\\Users\\jason\\OneDrive\\Documents\\TMU\\CPS40A\\IoT-Mutator'
+class TestSAC(unittest.TestCase):        
+    base_folder = os.path.join('C:', os.sep, 'Users', 'jason', 'OneDrive', 'Documents', 'TMU', 'CPS40A', 'IoT-Mutator')
+    test_prog = os.path.join(base_folder, 'SAC.txl')
+    
+    def test_Base(self):
+        test_case = os.path.join(self.base_folder, 'testsuite', 'Base.rules')
 
-    def test_SAC(self):
-        test_prog = f'{self.folder}\\SAC.txl'
-
-        # Test with no conditions
-        test_case = f'{self.folder}\\IoTB\\lock.rules'
-        test_result = subprocess.run(f'txl {test_case} {test_prog}', stdout=subprocess.PIPE)
-        actual_result = ('rule "unlock door in foyer"\r\nwhen\r\n    Item Foyer_Light changed to ON\r\nthen\r\n    '
-                         '{\r\n        Front_Door_Lock.postUpdate (ON)}\r\n\r\nend\r\n\r\nrule "turn off hallway light"'
-                         '\r\nwhen\r\n    Item Foyer_Light changed to ON\r\nthen\r\n    {\r\n        Front_Door_Lock.sendCommand (OFF)}'
-                         '\r\n\r\nend\r\n\r\n')
+        test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
+        actual_result = (
+                 'rule "Rule A"\r\n'
+                 'when\r\n'
+                 '    Item ItemA1 changed to ON\r\n'
+                 'then\r\n'
+                 '    ItemA2.postUpdate (ON)\r\n'
+                 'end\r\n'
+                 '\r\n'
+                 'rule "Rule B"\r\n'
+                 'when\r\n'
+                 '    Item ItemA1 changed to ON\r\n'
+                 'then\r\n'
+                 '    ItemA2.sendCommand (OFF)\r\n'
+                 'end\r\n'
+                 '\r\n')
+        
         self.assertEqual(test_result.stdout.decode(), actual_result)
 
-        # Test with single line condition
-        test_case = f'{self.folder}\\IoTB\\lock.rules'
-        test_result = subprocess.run(f'txl {test_case} {test_prog}', stdout=subprocess.PIPE)
-        actual_result = ('rule "unlock door in foyer"\r\nwhen\r\n    Item Foyer_Light changed to ON\r\nthen\r\n    '
-                         '{\r\n        Front_Door_Lock.postUpdate (ON)}\r\n\r\nend\r\n\r\nrule "turn off hallway light"'
-                         '\r\nwhen\r\n    Item Foyer_Light changed to ON\r\nthen\r\n    {\r\n        Front_Door_Lock.sendCommand (OFF)}'
-                         '\r\n\r\nend\r\n\r\n')
+    def test_SingleConditionSingleState(self):
+        test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionSingleState.rules')
+
+        test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
+        actual_result = (
+                 'rule "Rule A"\r\n'
+                 'when\r\n'
+                 '    Item ItemA1 changed to ON\r\n'
+                 'then\r\n'
+                 '    ItemA2.postUpdate (ON)\r\n'
+                 'end\r\n'
+                 '\r\n'
+                 'rule "Rule B"\r\n'
+                 'when\r\n'
+                 '    Item ItemA1 changed to ON\r\n'
+                 'then\r\n'
+                 '    ItemA2.sendCommand (OFF)\r\n'
+                 'end\r\n'
+                 '\r\n')
+
         self.assertEqual(test_result.stdout.decode(), actual_result)
 
 
