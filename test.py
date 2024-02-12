9bc69f61d66e17551d76c7b6fd97ad97aa410498
JasonQuantrill <91751222+JasonQuantrill@users.noreply.github.com>
2024-02-12 12:59:33 -0500

    test wtca base

--- test.py
+++ test.py
@@ -76,83 +76,83 @@ class TestSAC(unittest.TestCase):
 
 
 
-class TestWAC(unittest.TestCase):        
-    base_folder = os.path.join('C:', os.sep, 'Users', 'jason', 'OneDrive', 'Documents', 'TMU', 'CPS40A', 'IoT-Mutator')
-    test_prog = os.path.join(base_folder, 'WAC.txl')
+# class TestWAC(unittest.TestCase):        
+#     base_folder = os.path.join('C:', os.sep, 'Users', 'jason', 'OneDrive', 'Documents', 'TMU', 'CPS40A', 'IoT-Mutator')
+#     test_prog = os.path.join(base_folder, 'WAC.txl')
     
-    def test_Base(self):
-        test_case = os.path.join(self.base_folder, 'testsuite', 'Base.rules')
+#     def test_Base(self):
+#         test_case = os.path.join(self.base_folder, 'testsuite', 'Base.rules')
 
-        test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
+#         test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
         
-        actual_result = (
-                 'rule "Rule A"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 'if (ConditionA1 && ConditionA2)\r\n'
-                '{\r\n'
-                      'logInfo ("ItemA2", "Turning On")\r\n'
-                      'ItemA2.postUpdate (ON)\r\n'
-                '}\r\n'
-                 '    ItemA2.postUpdate (ON)\r\n'
-                 'end\r\n'
-                 '\r\n'
-                 'rule "Rule B"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    ItemA2.sendCommand (OFF)\r\n'
-                 'end\r\n'
-                 '\r\n')
+#         actual_result = (
+#                  'rule "Rule A"\r\n'
+#                  'when\r\n'
+#                  '    Item ItemA1 changed to ON\r\n'
+#                  'then\r\n'
+#                  'if (ConditionA1 && ConditionA2)\r\n'
+#                 '{\r\n'
+#                       'logInfo ("ItemA2", "Turning On")\r\n'
+#                       'ItemA2.postUpdate (ON)\r\n'
+#                 '}\r\n'
+#                  '    ItemA2.postUpdate (ON)\r\n'
+#                  'end\r\n'
+#                  '\r\n'
+#                  'rule "Rule B"\r\n'
+#                  'when\r\n'
+#                  '    Item ItemA1 changed to ON\r\n'
+#                  'then\r\n'
+#                  '    ItemA2.sendCommand (OFF)\r\n'
+#                  'end\r\n'
+#                  '\r\n')
         
-        self.assertEqual(test_result.stdout.decode(), actual_result)
+#         self.assertEqual(test_result.stdout.decode(), actual_result)
 
-    def test_SingleConditionSingleState(self):
-        test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionSingleState.rules')
+#     def test_SingleConditionSingleState(self):
+#         test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionSingleState.rules')
 
-        test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
-        actual_result = (
-                 'rule "Rule A"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    ItemA2.postUpdate (ON)\r\n'
-                 'end\r\n'
-                 '\r\n'
-                 'rule "Rule B"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    ItemA2.sendCommand (OFF)\r\n'
-                 'end\r\n'
-                 '\r\n')
+#         test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
+#         actual_result = (
+#                  'rule "Rule A"\r\n'
+#                  'when\r\n'
+#                  '    Item ItemA1 changed to ON\r\n'
+#                  'then\r\n'
+#                  '    ItemA2.postUpdate (ON)\r\n'
+#                  'end\r\n'
+#                  '\r\n'
+#                  'rule "Rule B"\r\n'
+#                  'when\r\n'
+#                  '    Item ItemA1 changed to ON\r\n'
+#                  'then\r\n'
+#                  '    ItemA2.sendCommand (OFF)\r\n'
+#                  'end\r\n'
+#                  '\r\n')
 
-        self.assertEqual(test_result.stdout.decode(), actual_result)
+#         self.assertEqual(test_result.stdout.decode(), actual_result)
 
-    def test_SingleConditionMultiState(self):
-        test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionMultiStates.rules')
+#     def test_SingleConditionMultiState(self):
+#         test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionMultiStates.rules')
         
-        test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
-        actual_result = (
-                 'rule "Rule A"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    logInfo ("ItemA2", "Turning On")\r\n'
-                 '    ItemA2.postUpdate (ON)\r\n'
-                 'end\r\n'
-                 '\r\n'
-                 'rule "Rule B"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    logInfo ("ItemB2", "Turning On")\r\n'
-                 '    ItemA2.sendCommand (OFF)\r\n'
-                 'end\r\n'
-                 '\r\n')
+#         test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
+#         actual_result = (
+#                  'rule "Rule A"\r\n'
+#                  'when\r\n'
+#                  '    Item ItemA1 changed to ON\r\n'
+#                  'then\r\n'
+#                  '    logInfo ("ItemA2", "Turning On")\r\n'
+#                  '    ItemA2.postUpdate (ON)\r\n'
+#                  'end\r\n'
+#                  '\r\n'
+#                  'rule "Rule B"\r\n'
+#                  'when\r\n'
+#                  '    Item ItemA1 changed to ON\r\n'
+#                  'then\r\n'
+#                  '    logInfo ("ItemB2", "Turning On")\r\n'
+#                  '    ItemA2.sendCommand (OFF)\r\n'
+#                  'end\r\n'
+#                  '\r\n')
 
-        self.assertEqual(test_result.stdout.decode(), actual_result)
+#         self.assertEqual(test_result.stdout.decode(), actual_result)
 
 
 
@@ -169,69 +169,64 @@ class TestWTCA(unittest.TestCase):
                  'when\r\n'
                  '    Item ItemA1 changed to ON\r\n'
                  'then\r\n'
-                 'if (ConditionA1 && ConditionA2)\r\n'
-                '{\r\n'
-                      'logInfo ("ItemA2", "Turning On")\r\n'
-                      'ItemA2.postUpdate (ON)\r\n'
-                '}\r\n'
-                 '    ItemA2.postUpdate (ON)\r\n'
+                 '    ItemB1.postUpdate (OFF)\r\n'
                  'end\r\n'
                  '\r\n'
                  'rule "Rule B"\r\n'
                  'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
+                 '    Item ItemB1 changed to OFF\r\n'
                  'then\r\n'
-                 '    ItemA2.sendCommand (OFF)\r\n'
+                 '    ItemB2.sendCommand (ON)\r\n'
                  'end\r\n'
                  '\r\n')
         
         self.assertEqual(test_result.stdout.decode(), actual_result)
 
-    def test_SingleConditionSingleState(self):
-        test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionSingleState.rules')
+    # def test_SingleConditionSingleState(self):
+    #     test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionSingleState.rules')
 
-        test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
-        actual_result = (
-                 'rule "Rule A"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    ItemA2.postUpdate (ON)\r\n'
-                 'end\r\n'
-                 '\r\n'
-                 'rule "Rule B"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    ItemA2.sendCommand (OFF)\r\n'
-                 'end\r\n'
-                 '\r\n')
+    #     test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
+    #     actual_result = (
+    #              'rule "Rule A"\r\n'
+    #              'when\r\n'
+    #              '    Item ItemA1 changed to ON\r\n'
+    #              'then\r\n'
+    #              '    ItemA2.postUpdate (ON)\r\n'
+    #              'end\r\n'
+    #              '\r\n'
+    #              'rule "Rule B"\r\n'
+    #              'when\r\n'
+    #              '    Item ItemA1 changed to ON\r\n'
+    #              'then\r\n'
+    #              '    ItemA2.sendCommand (OFF)\r\n'
+    #              'end\r\n'
+    #              '\r\n')
 
-        self.assertEqual(test_result.stdout.decode(), actual_result)
+    #     self.assertEqual(test_result.stdout.decode(), actual_result)
 
-    def test_SingleConditionMultiState(self):
-        test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionMultiStates.rules')
+    # def test_SingleConditionMultiState(self):
+    #     test_case = os.path.join(self.base_folder, 'testsuite', 'SingleConditionMultiStates.rules')
         
-        test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
-        actual_result = (
-                 'rule "Rule A"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    logInfo ("ItemA2", "Turning On")\r\n'
-                 '    ItemA2.postUpdate (ON)\r\n'
-                 'end\r\n'
-                 '\r\n'
-                 'rule "Rule B"\r\n'
-                 'when\r\n'
-                 '    Item ItemA1 changed to ON\r\n'
-                 'then\r\n'
-                 '    logInfo ("ItemB2", "Turning On")\r\n'
-                 '    ItemA2.sendCommand (OFF)\r\n'
-                 'end\r\n'
-                 '\r\n')
+    #     test_result = subprocess.run(['txl', test_case, self.test_prog], stdout=subprocess.PIPE)
+    #     actual_result = (
+    #              'rule "Rule A"\r\n'
+    #              'when\r\n'
+    #              '    Item ItemA1 changed to ON\r\n'
+    #              'then\r\n'
+    #              '    logInfo ("ItemA2", "Turning On")\r\n'
+    #              '    ItemA2.postUpdate (ON)\r\n'
+    #              'end\r\n'
+    #              '\r\n'
+    #              'rule "Rule B"\r\n'
+    #              'when\r\n'
+    #              '    Item ItemA1 changed to ON\r\n'
+    #              'then\r\n'
+    #              '    logInfo ("ItemB2", "Turning On")\r\n'
+    #              '    ItemA2.sendCommand (OFF)\r\n'
+    #              'end\r\n'
+    #              '\r\n')
 
-        self.assertEqual(test_result.stdout.decode(), actual_result)
+    #     self.assertEqual(test_result.stdout.decode(), actual_result)
 
 
 
